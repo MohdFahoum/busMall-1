2aa3c3590ccd4c41d1568fc85fc48c192bbc7e11
bomibear <bomibearly@gmail.com>
2019-06-17 13:14:36 -0700

    add HTML skeleton and start js

---
+++ js/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+Product.allProducts = [];
+
+
+var Product = function (name, src) {
+  this.name = name;
+  this.src = src;
+  this.clicks = 0;
+  this.views = 0;
+  Product.allProducts.push(this);
+};
